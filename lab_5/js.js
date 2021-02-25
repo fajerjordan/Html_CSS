86c5f6da3fd8145d0ae4637c890284261c8eb4ef
Majedah Hatamleh <MAJIDAHATAMLEH@OUTLOOK.COM>
2021-02-25 09:59:36 +0200

    update

---
+++ lab_5/js.js
@@ -0,0 +1,17 @@
+// for (var i = 0 ; i>0 ; i++) {
+
+// }
+
+
+for (i = 0; i < 9999; i++) {
+    var ask = prompt ("Do you love Audi Cars ??? it should be exactly yes, like me");
+    if (ask == 'yes') {
+        break; 
+    }
+
+  }
+
+  var number = prompt ("How much you love it ?? give a number then "); 
+  for (i = 0; i < number; i++) {
+    document.write('<img src="https://images.unsplash.com/photo-1541348263662-e068662d82af?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YXVkaXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80">');
+}
